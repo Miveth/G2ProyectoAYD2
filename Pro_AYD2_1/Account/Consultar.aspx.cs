8737f04d2d41874fef9429e98db24593b2dddca6
Erick Dávila <erick.e.davila@gmail.com>
2018-05-11 02:06:50 -0600

    Final Consulta -Erick

--- Pro_AYD2_1/Account/Consultar.aspx.cs
+++ Pro_AYD2_1/Account/Consultar.aspx.cs
@@ -30,6 +30,30 @@ namespace Pro_AYD2_1.Account
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlDataAdapter sda = new SqlDataAdapter("select * from usuario where cuenta = '" + Session["cuenta"].ToString()+ "';", cn.conexion);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+
+
+            if (dt.Rows.Count.ToString() == "1")
+            {
+                DataRow row = dt.Rows[0];
+                cuenta.Text = Session["cuenta"].ToString();
+                nombre.Text = Session["user"].ToString();
+                monto.Text = row["saldoIni"].ToString();
+                apellido.Text = row["apellido"].ToString();
+                mensaje.Text = "Consulta realizada con exito!!!";
+
+            }
+
+            else
+            {
+                mensaje.Text = "Intento Fallido";
+                mensaje.Visible = true;
+
+            }
             
         }
 
