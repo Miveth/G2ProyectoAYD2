80ec021ee796b20805e7d27ae0e268e70c1327fc
568robin8 <abr568@gmail.com>
2018-05-11 02:42:17 -0600

    prueba unitaria

---
+++ Pro_AYD2_1.Tests/conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pro_AYD2_1.Tests
+{
+    [TestClass]
+    public class conexion
+    {
+        [TestMethod]
+        public void Testconexion()
+        {
+            DBComun d = new DBComun();
+            Boolean test = true;
+            Boolean esperado = d.conexcion_establecida;
+
+
+            Assert.AreEqual(test, esperado);
+        }
+    }
+}
--- Pro_AYD2_1/DBComun.cs
+++ Pro_AYD2_1/DBComun.cs
@@ -10,12 +10,22 @@ namespace Pro_AYD2_1
     {
 
         public SqlConnection conexion;
+        public Boolean conexcion_establecida = false;
 
         public DBComun()
         {
 
             conexion = new SqlConnection("Data Source=DESKTOP-9NOAC3K;Initial Catalog=Prueba;Integrated Security=True");
+            try
+            {
+                insertardatos("Select * from usuario;");
+                conexcion_establecida = true;
+            }
+            catch (Exception e)
+            {
+                conexcion_establecida = true;
 
+            }
         }
         public SqlConnection getconexion
         {
