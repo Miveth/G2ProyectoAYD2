83f8ad6b95f9ffa88cbc113c47067cf29fd88f1b
568robin8 <abr568@gmail.com>
2018-05-11 01:47:39 -0600

    terminando transaccion erick

--- Pro_AYD2_1/Account/WebForm2.aspx.cs
+++ Pro_AYD2_1/Account/WebForm2.aspx.cs
@@ -33,11 +33,36 @@ namespace Pro_AYD2_1.Account
             Response.Redirect("/");
         }
 
+
         protected void transferencia_Click(object sender, EventArgs e)
         {
 
-           
-            
+            SqlDataAdapter sda = new SqlDataAdapter("select * from usuario where cuenta = '" + Session["cuenta"].ToString() + "' and saldoIni >= '" + monto.Text + "';", cn.conexion);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+
+
+            if (dt.Rows.Count.ToString() == "1")
+            {
+                cadena = "INSERT INTO transaccion ( monto, cuenta) values('" + monto.Text + "','" + cuenta.Text + "'); ";
+                cn.insertardatos(cadena);
+                mensaje.Text = "Transaccion Realizada con exito!!!";
+
+                cadena = "update usuario set saldoIni = saldoIni - '" + monto.Text + "'from usuario u where u.cuenta = '" + Session["cuenta"].ToString() + "'; ";
+                cn.insertardatos(cadena);
+
+                cadena = "update usuario set saldoIni = saldoIni + '" + monto.Text + "'from usuario u where u.cuenta = '" + cuenta.Text + "'; ";
+                cn.insertardatos(cadena);
+            }
+
+            else
+            {
+                mensaje.Text = "Lo sentimos, Compruebe si la cuenta existe o si tiene suficiente saldo para completar la transaccion";
+                mensaje.Visible = true;
+
+            }
 
         }
 
